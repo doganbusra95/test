6ed6a876d12379f742ed6855425b106e8a6e7eee
doganbusra95 <doganbusra.95@outlook.com>
2020-02-04 10:32:18 +0300

    test
    deneme tamam

---
+++ ConsoleApp1_deneme/ConsoleApp1_deneme/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleApp1_deneme
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Merhaba World!");
+        }
+    }
+}
